5494db4b22d766305e53ba4554e68157504e26d7
shinkensen <noobywinner587@gmail.com>
2025-10-28 21:37:52 -0500

    did some more, working on login functionality

--- signin.js
+++ signin.js
@@ -1,3 +1,20 @@
+import {createClient} from 'https://esm.sh/@supabase/supabase-js';
+const supabase= createClient("https://rsaquxvrhyudikhkviti.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzYXF1eHZyaHl1ZGlraGt2aXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNTM3MzMsImV4cCI6MjA3NjgyOTczM30.py5-5y8MO4foxXPHvwzgHKlW2Q0PIqQ2ktGwIPRwEWs")
+
+
+
+async function signUp(email,pass) {
+    const {data,error} = await supabase.auth.signInWithPassword({
+        email: email, password: pass
+    })
+    if (error){
+        return [false, error.message];
+    }
+    else{
+    }
+}
+
+
 const go=document.getElementById("go");
 const userInput= document.getElementById("user");
 const passInput= document.getElementById("pass");
